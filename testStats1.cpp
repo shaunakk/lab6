9e774be4503b7e4c4947ad69b0a60eee4d88d776
Shaunak Kale <epicshaunak@gmail.com>
2021-05-26 21:57:40 -0700

    hi

--- testStats1.cpp
+++ testStats1.cpp
@@ -4,24 +4,24 @@
 #include "tddFuncs.h"
 using namespace std;
 
-int main() {
-
+int main()
+{
 
   cout << "Testing stats" << endl;
 
-  vector<double> x = vector<double>{43.0, 21, 25, 42, 57, 59}; 
-  vector<double> y = vector<double>{99.0, 65, 79, 75, 87, 81}; 
+  vector<double> x = vector<double>{43.0, 21, 25, 42, 57, 59};
+  vector<double> y = vector<double>{99.0, 65, 79, 75, 87, 81};
 
-  double meanX =  stats::computeMean(x); 
-  double stdDevX = stats::computeStdDevSample(x);  
-  double meanY =  stats::computeMean(y); 
-  double stdDevY = stats::computeStdDevSample(y); 
+  double meanX = stats::computeMean(x);
+  double stdDevX = stats::computeStdDevSample(x);
+  double meanY = stats::computeMean(y);
+  double stdDevY = stats::computeStdDevSample(y);
 
   ASSERT_EQUALS(meanX, 41.167);
   //TODO fill in!!!
-  //ASSERT_EQUALS(stdDevX, ?);
-  //ASSERT_EQUALS(meanY, ?);
-  //ASSERT_EQUALS(stdDevY, ?);
+  ASSERT_EQUALS(stdDevX, 15.753);
+  ASSERT_EQUALS(meanY, 81.0);
+  ASSERT_EQUALS(stdDevY, 11.454);
 
   return 0;
 }
